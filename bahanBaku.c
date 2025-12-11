ed405fda3bf08ca1d3f04eb587ed59cc4990038b
ikiiiky <kiuarrasyid27@gmail.com>
2025-12-11 07:13:43 +0700

    create, read, update, delate sistem meja

---
+++ bahanBaku.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "bahanBaku.h"
+
+#define MAXX 100
+
+BahanBaku databahan[MAXX];
+int jumlahBahanBaku = 0;
+
+void TambahBahanBaku() {
+
+}
---
+++ bahanBaku.h
@@ -0,0 +1,13 @@
+#ifndef EATBOX_BAHANBAKU_H
+#define EATBOX_BAHANBAKU_H
+#include "data.h"
+
+void tambahBahanBaku();
+void simpanDataBahanBaku();
+void TampilBahanBaku();
+void tambahBahanBaku();
+void updateStockBahanBaku();
+
+
+
+#endif //EATBOX_BAHANBAKU_H
